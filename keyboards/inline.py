a70d62cd731bee7d367d54a1eb9dc9331eb091c6
Unknown_8779 <erkinovashahlo230@gmail.com>
2026-03-16 17:25:02 +0500

    update

--- keyboards/inline.py
+++ keyboards/inline.py
@@ -14,3 +14,11 @@ def user_action(user_id):
             [InlineKeyboardButton(text="admin",callback_data=f'changeto_admin_{user_id}'),InlineKeyboardButton(text="user",callback_data=f'changeto_user_{user_id}')]
         ]
     )
+
+def product_inline(products):
+    keyboard=[]
+
+    for product in products:
+        keyboard.append([InlineKeyboardButton(text=f"{product["name"] } ({product["price"]} so'm)",callback_data=f"product_{product["id"]}")])
+    
+    return InlineKeyboardMarkup(inline_keyboard=keyboard)
